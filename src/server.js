7d5786b6f781e30f1044630a1435be1784e37ea5
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-09-02 19:28:26 -0400

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const { createServer, plugins } = require('restify');
+const corsMiddleware = require('restify-cors-middleware');
 const components = require('./components');
 
+const cors = corsMiddleware({
+  origins: ['*'],
+});
+
 const server = createServer({ name: 'SmartPlug' });
+server.pre(cors.preflight);
+server.use(cors.actual);
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 server.use(plugins.bodyParser());
+
 components(server);
 
 exports.server = server;
